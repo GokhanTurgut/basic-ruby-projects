c77d221aadfe17c7735c75535df21415aa878bd7
Gokhan T <trgtgokhan@gmail.com>
2022-04-22 23:56:45 +0300

    Add ceasar cipher solution

---
+++ caesar_cipher/caesar_cipher.rb
@@ -0,0 +1,18 @@
+# @param string [String] input string to be encrypted
+# @param shift [Integer] number of places to shift each char
+# @return [String] encrypted string
+def caesar_cipher(string, shift)
+  encrypted_string = ""
+  string.each_char do |char|
+    if char.ord >= 65 && char.ord <= 90
+      encrypted_string += ((char.ord + shift - 65) % 26 + 65).chr
+    elsif char.ord >= 97 && char.ord <= 122
+      encrypted_string += ((char.ord + shift - 97) % 26 + 97).chr
+    else
+      encrypted_string += char
+    end
+  end
+  encrypted_string
+end
+
+puts caesar_cipher("What a string!", 5)
