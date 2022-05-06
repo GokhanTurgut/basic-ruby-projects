ef34f00956ee38f75db271cc62dff198741850f8
Gokhan T <trgtgokhan@gmail.com>
2022-05-06 20:37:41 +0300

    Add names of legislators with google civic api search

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -1,6 +1,28 @@
 # frozen_string_literal: true
 
 require 'csv'
+require 'google/apis/civicinfo_v2'
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, '0')[0..4]
+end
+
+def legislators_by_zipcode(zip)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+  begin
+    legislators = civic_info.representative_info_by_address(
+      address: zip,
+      levels: 'country',
+      roles: %w[legislatorUpperBody legislatorLowerBody]
+    )
+    legislators = legislators.officials
+
+    legislators.map(&:name).join(', ')
+  rescue StandardError
+    'Wrong zipcode!'
+  end
+end
 
 puts 'EventManager initialized.'
 puts '--------------------------'
@@ -13,6 +35,10 @@ contents = CSV.open(
 
 contents.each do |row|
   name = row[:first_name]
-  zipcode = row[:zipcode]
-  puts "#{name}, #{zipcode}"
+
+  zipcode = clean_zipcode(row[:zipcode])
+
+  legislators = legislators_by_zipcode(zipcode)
+
+  puts "#{name} | #{zipcode} | #{legislators}"
 end
