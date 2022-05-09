655b84847fac3e626a61e4b0f14db8843b522b11
Gokhan T <trgtgokhan@gmail.com>
2022-05-09 22:11:36 +0300

    Add custom each, each_with_index and select

--- custom_enumerables/lib/my_enumerables.rb
+++ custom_enumerables/lib/my_enumerables.rb
@@ -1,11 +1,27 @@
+# frozen_string_literal: true
+
 module Enumerable
-  # Your code goes here
+  def my_each_with_index
+    for index in 0..length - 1
+      yield(self[index], index)
+    end
+    self
+  end
+
+  def my_select
+    selected = []
+    for element in self
+      selected.push(element) if yield(element)
+    end
+    selected
+  end
 end
 
-# You will first have to define my_each
-# on the Array class. Methods defined in
-# your enumerable module will have access
-# to this method
 class Array
-  # Define my_each here
+  def my_each
+    for element in self
+      yield(element)
+    end
+    self
+  end
 end
