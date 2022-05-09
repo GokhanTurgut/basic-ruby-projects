230d7ddc520e0a1697c1622caadbb4b20e048b25
Gokhan T <trgtgokhan@gmail.com>
2022-05-10 01:26:55 +0300

    Add custom all?, none? and count

--- custom_enumerables/lib/my_enumerables.rb
+++ custom_enumerables/lib/my_enumerables.rb
@@ -2,7 +2,7 @@
 
 module Enumerable
   def my_each_with_index
-    for index in 0..length - 1
+    for index in 0..size - 1
       yield(self[index], index)
     end
     self
@@ -14,6 +14,30 @@ module Enumerable
       selected.push(element) if yield(element)
     end
     selected
+  end
+
+  def my_all?
+    for element in self
+      return false unless yield(element)
+    end
+    true
+  end
+
+  def my_none?
+    for element in self
+      return false if yield(element)
+    end
+    true
+  end
+
+  def my_count
+    return size unless block_given?
+
+    counter = 0
+    for element in self
+      counter += 1 if yield(element)
+    end
+    counter
   end
 end
 
